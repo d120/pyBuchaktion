e4ff1b53c53c56f6183d9d4e2be1d6e6f539059d
Daniel Schweighöfer <SchweighoeferD@googlemail.com>
2017-01-06 17:48:31 +0100

    include db tables in admin interface

--- pyBuchaktion/admin.py
+++ pyBuchaktion/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from . import models
+
+admin.site.register(models.Book)
+admin.site.register(models.Order)
+admin.site.register(models.Student)
+admin.site.register(models.OrderTimeframe)
+admin.site.register(models.Semester)
+admin.site.register(models.TucanModule)
