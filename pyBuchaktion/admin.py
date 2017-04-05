1aed5c4c898f7c5df719fcc1164df837d7b7a8e5
Daniel Schweighöfer <SchweighoeferD@googlemail.com>
2017-04-05 11:30:01 +0200

    Add comments to admin

--- pyBuchaktion/admin.py
+++ pyBuchaktion/admin.py
@@ -18,6 +18,12 @@ from . import models
 @admin.register(models.Book)
 class BookAdmin(admin.ModelAdmin):
 
+    """
+        The book admin displays title, author and isbn of a book,
+        as well as the number of order for this book.
+    """
+
+    # The columns that are displayed in the list view
     list_display = (
         'title',
         'author',
@@ -25,15 +31,18 @@ class BookAdmin(admin.ModelAdmin):
         'number_of_orders',
     )
 
+    # The keys that the list can be filtered by
     list_filter = (
         'state',
     )
 
+    # Annotate the queryset with the number of orders.
     def get_queryset(self, request):
         qs = super(BookAdmin, self).get_queryset(request)
         qs = qs.annotate(Count('order'))
         return qs
 
+    # The number of orders for this book
     def number_of_orders(self, book):
         return book.order__count
 
@@ -41,22 +50,42 @@ class BookAdmin(admin.ModelAdmin):
     number_of_orders.short_description = _("orders")
 
 #class OrderResource(resources.ModelResource):
-
+#
 #    class Meta:
 #        model = models.Order
 
 @admin.register(models.Order)
 #class OrderAdmin(ImportExportActionModelAdmin):
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('book_title', 'student', 'timeframe')
-    list_filter = ('status', 'order_timeframe')
+
+    """
+        The admin for the orders, displaying the title of the ordered book,
+        the student ordering the book and the timeframe.
+    """
+
+    # The columns that are displayed
+    list_display = (
+        'book_title',
+        'student',
+        'timeframe',
+    )
+
+    # The keys that the list can be filtered by
+    list_filter = (
+        'status',
+        'order_timeframe',
+    )
+
+    # The actions that can be triggered on orders
     actions = ["export"]
 
+    # The title of the book to be ordered
     def book_title(self, order):
         return order.book.title
 
     book_title.short_description = _("title")
 
+    # The timeframe to which this order belongs
     def timeframe(self, order):
         start = _("{:%Y-%m-%d}").format(order.order_timeframe.start_date)
         end = _("{:%Y-%m-%d}").format(order.order_timeframe.end_date)
@@ -64,6 +93,7 @@ class OrderAdmin(admin.ModelAdmin):
 
     timeframe.short_description = _("order timeframe")
 
+    # The admin action for exporting to custom CSV
     def export(self, request, queryset):
 
         out_stream = io.StringIO()
@@ -87,17 +117,45 @@ class OrderAdmin(admin.ModelAdmin):
 
 @admin.register(models.Student)
 class StudentAdmin(admin.ModelAdmin):
+
+    """
+        The admin for students.
+    """
     pass
 
 @admin.register(models.OrderTimeframe)
 class OrderTimeframeAdmin(admin.ModelAdmin):
-    list_display = ('end_date', 'start_date', 'semester')
+
+    """
+        The admin for order timeframes displays the start and end dates and
+        the semester the timeframe belongs to.
+    """
+
+    # The columns that are displayed in the list view
+    list_display = (
+        'end_date',
+        'start_date',
+        'semester',
+    )
 
 @admin.register(models.Semester)
 class SemesterAdmin(admin.ModelAdmin):
+
+    """
+        The admin for a semester.
+    """
+
     #radio_fields = {"season": admin.VERTICAL}
     pass
 
 @admin.register(models.TucanModule)
 class ModuleAdmin(admin.ModelAdmin):
-    list_display = ('name', 'module_id')
+
+    """
+        The admin for a module displays the name and module id.
+    """
+
+    list_display = (
+        'name',
+        'module_id',
+    )
