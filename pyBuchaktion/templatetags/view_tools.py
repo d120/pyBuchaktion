04558a377331b7bee43989664da21fe62e974861
Daniel Schweighöfer <SchweighoeferD@gmail.com>
2017-04-07 19:47:20 +0200

    Cleanup more template filters

--- pyBuchaktion/templatetags/view_tools.py
+++ pyBuchaktion/templatetags/view_tools.py
@@ -1,82 +1,80 @@
 from pyBuchaktion.models import Book
 from django import template
+from django.http import QueryDict
 from django.utils.translation import ugettext_lazy as _
 
 register = template.Library()
 
-BOOK_STATE_CLASSES = [
-    ('AC', 'default'),
-    ('RJ', 'danger'),
-    ('PP', 'info'),
-    ('OL', 'warning'),
-]
+BOOK_STATE_CLASSES = {
+    'AC': 'default',
+    'RJ': 'danger',
+    'PP': 'info',
+    'OL': 'warning',
+}
 
+@register.filter
 def get_state_class(state):
-    return [(s, v) for s, v in BOOK_STATE_CLASSES if s == state][0][1]
+    return BOOK_STATE_CLASSES.get(state)
 
 
-BOOK_STATE_TEXTS = [
-    ('AC', ""),
-    ('RJ', _("This book has been rejected, and may not be ordered")),
-    ('PP', _("This book has been proposed, but has yet to be confirmed by our team. "
-        "Until then it may not be ordered.")),
-    ('OL', _("This book has been marked obsolete, a new version is available. It may not be ordered")),    
-]
+BOOK_STATE_TEXTS = {
+    'AC': "",
+    'RJ': _("This book has been rejected, and may not be ordered"),
+    'PP': _("This book has been proposed, but has yet to be confirmed by our team. "
+            "Until then it may not be ordered."),
+    'OL': _("This book has been marked obsolete, a new version is available. It may not be ordered"),
+}
 
+@register.filter
 def get_state_text(state):
-    return [(s, t) for s, t in BOOK_STATE_TEXTS if s == state][0][1]
+    return BOOK_STATE_TEXTS.get(state)
 
-ORDER_STATUS_CLASSES = [
-    ('PD', 'info'),
-    ('OD', 'success'),
-    ('RJ', 'danger'),
-    ('AR', 'info'),
-]
+ORDER_STATUS_CLASSES = {
+    'PD': 'info',
+    'OD': 'success',
+    'RJ': 'danger',
+    'AR': 'info',
+}
 
+@register.filter
 def get_status_class(status):
-    return [(s, v) for s, v in ORDER_STATUS_CLASSES if s == status][0][1]
+    return ORDER_STATUS_CLASSES.get(status)
 
+ORDER_STATUS_TEXTS = {
+    'PD': _("This book is marked for, but has not yet been ordered from our merchant. "
+            "Until then, the order can be aborted at any time. We'll inform you when "
+            "we ordered or finally rejected it."),
+    'OD': _("This book has been ordered from our merchant. We'll inform you when it arrived."),
+    'RJ': _("This order has been rejected."),
+    'AR': _("The order was fulfilled, the book is now available."),
+}
 
-ORDER_STATUS_TEXTS = [
-    ('PD', _("This book is marked for, but has not yet been ordered from our merchant. "
-        "Until then, the order can be aborted at any time. We'll inform you when we ordered "
-        "or finally rejected it.")),
-    ('OD', _("This book has been ordered from our merchant. We'll inform you when it arrived.")),
-    ('RJ', _("This order has been rejected.")),
-    ('AR', _("The order was fulfilled, the book is now available.")),
-]
-
+@register.filter
 def get_status_text(status):
-    return [(s, t) for s, t in ORDER_STATUS_TEXTS if s == status][0][1]
+    return ORDER_STATUS_TEXTS.get(status)
 
-register.filter('get_state_class', get_state_class)
-register.filter('get_state_text', get_state_text)
+@register.filter
+def dict_unset(dct, key):
+    _dct = dct.copy()
+    if (key in _dct):
+        _dct.pop(key)
+    return _dct
 
-register.filter('get_status_class', get_status_class)
-register.filter('get_status_text', get_status_text)
+@register.filter
+def dict_join(value, param):
+    dct = value.copy()
+    for key, val in param.items():
+        dct[key] = val
+    return dct
 
+@register.filter
+def as_dict(value, param):
+    return {param: value}
 
-@register.inclusion_tag('url.html')
-def url_set(params, key, value, drop = None):
-    params_ = params.copy()
-    params_[key] = value
-
-    if (drop != None and drop in params_):
-        params_.pop(drop)
-
-    url = "?" + params_.urlencode()
-    return {"url": url}
-
-@register.inclusion_tag('url.html')
-def url_unset(params, key):
-    params_ = params.copy()
-    if key in params_:
-        params_.pop(key)
-    
-    # always return at least '?' to have a valid link
-    url = "?" + params_.urlencode()
-
-    return {"url": url}
+# This only works for QueryDicts!
+@register.filter
+def urlencode(value):
+    return "?" + value.urlencode()
 
 @register.inclusion_tag('pyBuchaktion/list_limits.html')
 def list_limits(params, current, options, default):
