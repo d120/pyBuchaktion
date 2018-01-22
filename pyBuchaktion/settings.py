f1a735eaf3bf87da094ae3d7c629be50cb25a24b
Daniel Schweighöfer <SchweighoeferD@gmail.com>
2018-01-22 11:16:49 +0100

    Reconfigure for new cas server

--- pyBuchaktion/settings.py
+++ pyBuchaktion/settings.py
@@ -1,4 +1,4 @@
 from django.conf import settings
 
-BUCHAKTION_STUDENT_LDAP_GROUP = getattr(settings, 'BUCHAKTION_STUDENT_LDAP_GROUP', "cn=_fb_20,ou=STUD,o=TU")
+BUCHAKTION_STUDENT_LDAP_GROUP = getattr(settings, 'BUCHAKTION_STUDENT_LDAP_GROUP', "FB20")
 BUCHAKTION_MESSAGES_DEBUG = getattr(settings, 'BUCHAKTION_MESSAGES_DEBUG', True)
